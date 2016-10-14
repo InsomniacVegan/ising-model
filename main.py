8f3804bbb7828ce96251cfbcade23456a8780274
InsomniacVegan <luke.e.elliott@gmail.com>
2016-10-14 19:25:00 +0100

    Added direct parameter control

--- main.py
+++ main.py
@@ -46,19 +46,55 @@ def main(stdscr):
         time.sleep(0.01)
         stdscr.refresh()
 
+        # Input
         c = stdscr.getch()
+
+        # Temperature
         if c == curses.KEY_UP:
             model.update_params(np.around(model.temp+0.1, 1), model.ext_field)
         elif c == curses.KEY_DOWN:
             if model.temp == 0.1: continue
             model.update_params(np.around(model.temp-0.1,1 ), model.ext_field)
+        elif c == ord('t'):
+            stdscr.nodelay(False)
+            stdscr.addstr(36, 0, 'Set T: ')
+            stdscr.refresh()
+
+            curses.echo()
+            temp_in_str = stdscr.getstr(36, 7, 10)
+            model.update_params(float(temp_in_str.strip()), model.ext_field)
+
+            curses.noecho()
+            stdscr.nodelay(True)
+            stdscr.move(36, 0)
+            stdscr.clrtoeol()
+
+        # External field
         elif c == curses.KEY_RIGHT:
             model.update_params(model.temp, np.around(model.ext_field+0.1,1))
         elif c == curses.KEY_LEFT:
             model.update_params(model.temp, np.around(model.ext_field-0.1,1))
+        elif c == ord('h'):
+            stdscr.nodelay(False)
+            stdscr.addstr(36, 0, 'Set H: ')
+            stdscr.refresh()
+
+            curses.echo()
+            ext_in_str = stdscr.getstr(36, 7, 10)
+            model.update_params(model.temp, float(ext_in_str.strip()))
+
+            curses.noecho()
+            stdscr.nodelay(True)
+            stdscr.move(36,0)
+            stdscr.clrtoeol()
+
+        # Quit
         elif c == ord('q'):
             exit()
 
+        for i in range(28,37):
+            stdscr.move(i, 0)
+            stdscr.clrtoeol()
         model.evolve_model()
 
 
